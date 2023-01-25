e19b5a1950deb64f9045f3bf56e5a8e891a0cfbd
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-25 16:39:25 +0300

    implemented function that parses command to separate commands

--- src/parse_command.rs
+++ src/parse_command.rs
@@ -4,23 +4,16 @@
 /// 
 /// ```rust
 /// let input = "echo hello && echo world; echo a > text.txt";
-/// let commands = parse_input();
+/// let commands = parse_commands(input);
 /// 
-/// assert_eq!(commands, vec![vec![vec!["echo", "hello"], vec!["echo", "world"]], vec![vec!["echo", "a", ">", "text.txt"]]])
+/// assert_eq!(commands, vec![vec!["echo hello", "echo world"], vec![" echo a > text.txt"]])
 /// ```
 /// 
-pub fn parse_command(command: &str) -> Vec<Vec<Vec<&str>>> {
-    let mut separate_commands = Vec::new();
+pub fn parse_command(command: &str) -> Vec<Vec<&str>> {
+    let mut separate_commands: Vec<Vec<&str>> = Vec::new();
 
     for independent_command in command.split(";").collect::<Vec<&str>>() {
-        let mut independent_commands: Vec<Vec<&str>> = Vec::new();
-
-        for single_command in independent_command.split("&&") {
-            let single_command_tokens = single_command.split_whitespace().collect();
-            independent_commands.push(single_command_tokens);
-        }
-
-        separate_commands.push(independent_commands);
+        separate_commands.push(independent_command.split(" && ").collect());
     }
     separate_commands
 }
