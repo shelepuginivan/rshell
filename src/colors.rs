19c38f003c149c216ec5b10cd74072f1021bcd58
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-27 16:32:07 +0300

    added bold ascii escape

--- src/colors.rs
+++ src/colors.rs
@@ -3,6 +3,8 @@
 pub const RED: &str  = "\x1b[31m";
 pub const GREEN: &str = "\x1b[32m";
 
+pub const BOLD: &str = "\x1b[1m";
+
 pub const RESET: &str = "\x1b[0m";
 
 pub fn red(text: &str) -> String {
