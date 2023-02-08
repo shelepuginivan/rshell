50db521c59aad560f5f2c51b65ec4f40237f04c8
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-08 03:49:46 +0300

    implemented function declaration

--- src/builtins.rs
+++ src/builtins.rs
@@ -1,6 +1,8 @@
-use std::env::{set_current_dir, set_var, var};
+use std::env::{set_current_dir, set_var, var, current_dir};
 use std::error::Error;
+use std::io::{Write, stdout};
 use std::path::Path;
+use std::str::SplitWhitespace;
 use crate::execute::ExecutionResult;
 use crate::utils::parse_single_argument;
 
@@ -91,4 +93,57 @@ pub fn set_alias(expression: Option<&str>) -> ExecutionResult {
     set_var(name, value);
 
     ExecutionResult::Success
+}
+
+pub fn function_declaration(args: SplitWhitespace) -> ExecutionResult {
+    let mut tokens = args;
+
+    let function_name = match tokens.next() {
+        Some(name) => name,
+        None => return ExecutionResult::Error(Box::<dyn Error>::from("function name required"))
+    };
+
+    let mut function_body = String::new();
+
+    if tokens.next().unwrap() == "{" {
+        let cwd_path = current_dir().unwrap_or_default();
+        let cwd = cwd_path.to_str().unwrap_or_default();
+
+        let prompt = " ".repeat(cwd.len() + 3) + "> ";
+
+        print!("{prompt}");
+
+        loop {
+            match stdout().flush() {
+                Ok(_) => {},
+                Err(err) => return ExecutionResult::Error(Box::from(err))
+            };
+
+            let mut line = String::new();
+
+            print!("{prompt}");
+
+            match std::io::stdin().read_line(&mut line) {
+                Ok(_) => {
+                    if line.trim() == "}" {
+                        match stdout().flush() {
+                            Ok(_) => {},
+                            Err(err) => return ExecutionResult::Error(Box::from(err))
+                        };
+                        
+                        break;
+                    };
+        
+                    function_body += &line;
+                },
+                Err(err) => return ExecutionResult::Error(Box::from(err))
+            };
+        }
+    } else {
+        function_body = tokens.collect::<Vec<&str>>().join(" ");
+    };
+
+    set_var("__FN_".to_owned() + function_name, function_body);
+
+    return ExecutionResult::Success;
 }
